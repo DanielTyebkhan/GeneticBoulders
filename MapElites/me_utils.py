4c3918a4ef43fd865481150ffaddf689222f289f
Daniel Tyebkhan <dtyebkhan@gmail.com>
2022-10-24 11:17:35 -0400

    wrote route conversion to and from me params

--- MapElites/me_utils.py
+++ MapElites/me_utils.py
@@ -13,6 +13,11 @@ class MEParams:
     iterations: int
 
 
+ROUTE_START_COUNT_I = 0
+ROUTE_END_COUNT_I = 3
+ROUTE_MID_COUNT_I = 6
+
+
 def route_to_ME_params(route: MoonBoardRoute):
     """
     Format:
@@ -38,3 +43,15 @@ def route_to_ME_params(route: MoonBoardRoute):
     arr += [-1 for i in range(MoonBoardRoute.MAX_HOLDS - route.num_holds())]
     return arr
     
+
+def ME_params_to_route(params: List[int]) -> MoonBoardRoute:
+    si = ROUTE_START_COUNT_I
+    ei = ROUTE_END_COUNT_I
+    mi = ROUTE_MID_COUNT_I
+    n_start = params[si]
+    n_end = params[ei]
+    n_mid = params[mi]
+    start_holds = MoonBoardRoute.valid_indices_to_holds(params[si + 1: si + n_start + 1])
+    mid_holds = MoonBoardRoute.valid_indices_to_holds(params[mi + 1: mi + n_mid + 1])
+    end_holds = MoonBoardRoute.valid_indices_to_holds(params[ei + 1: ei + n_end + 1])
+    return MoonBoardRoute(start_holds=start_holds, mid_holds=mid_holds, end_holds=end_holds)
