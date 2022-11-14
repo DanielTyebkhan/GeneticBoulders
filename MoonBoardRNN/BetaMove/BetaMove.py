bb4894c184ee5218e20fd72d582e11dfa75f911a
tyebkhad <tyebkhad@union.edu>
2022-11-14 08:11:56 -0500

    added matrix normalization

--- MoonBoardRNN/BetaMove/BetaMove.py
+++ MoonBoardRNN/BetaMove/BetaMove.py
@@ -38,6 +38,30 @@ def route_to_x_vectors(route: MoonBoardRoute):
 	x_vectors = beta_to_x_vectors(beta)
 	return x_vectors
 
+def normalization(matrix):
+	tmax = matrix.shape[0]
+
+	mu_x = 5.0428571
+	sig_x = 3.079590
+	mu_y = 9.8428571
+	sig_y = 4.078289957
+	mu_hand = 4.2428571
+	sig_hand = 2.115829552
+	mu_diff = 12.118308
+	sig_diff = 11.495348196
+
+	mu_vec = np.array([mu_x, mu_y, 0, mu_hand, mu_x, mu_y, mu_hand, 0, 0, mu_x, mu_y, mu_hand, 0, 0, 0, 0, 0, 0, 0, 0, 0, mu_diff])
+	sig_vec = np.array([sig_x, sig_y, 1, sig_hand, sig_x, sig_y, sig_hand, sig_x, sig_y, sig_x, sig_y, sig_hand, sig_x, sig_y, 1, 1, 1, 1, 1, 1, 1, sig_diff])
+    
+	mask = np.zeros_like(matrix)
+	mask[0:int(tmax), :] = 1
+	normalized = np.copy(matrix)
+	normalized -= mu_vec
+	normalized /= sig_vec
+	normalized *= mask
+
+	return normalized
+
 def load_feature_dict():
     dirname = pathlib.Path(__file__).parent
     file_path = os.path.join(dirname, 'HoldFeature2016.xlsx')
