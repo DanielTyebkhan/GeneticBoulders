c3d8d06dc1caf6817d3a28ba167473dfb55c51c8
Daniel Tyebkhan <dtyebkhan@gmail.com>
2022-11-17 11:04:01 -0500

    update descriptors

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import random
 from share.moonboard_route import MoonBoardRoute
 from MapElites.me_utils import MEParams
 import MapElites.ribs_interface as me
+from util import save_pickle
 
 def main():
     random.seed(4114)
     params = MEParams(
-        (6, 6), [(1, 7), (0, 6)], 50, 0.01, 4, 200)
-    for i in range(3):
+        (6, 6), [(1, 7), (0, 6)], 10, 0.01, 5, 100)
+    tasks = 10
+    loggers = []
+    for i in range(9):
         print(f'On experiment {i}')
-        me.run_mapelites(target_grade='V4', params=params, report_frequency=10, save_path='results')
+        loggers.append(me.run_mapelites(target_grade='V4', params=params, report_frequency=10, save_path='results'))
+    save_pickle(loggers, 'results/aggregate.p')
 
 if __name__ == '__main__':
     main()
