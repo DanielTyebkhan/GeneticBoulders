a221abcefb29f8709df6b85b501b45a7cf363b26
tyebkhad <tyebkhad@union.edu>
2023-01-19 18:10:08 -0500

    run updates

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from MapElites.me_utils import MEParams
 import MapElites.ribs_interface as me
 
 def main():
-    num_reps = 6
-    ind_per_emitter = 2
-    iterations = 10
+    num_reps = 30
+    population_size = 50
+    num_emitters = 5
+    batch_size = population_size // num_emitters
+    iterations = 500
     params = MEParams(
-        (6, 6), [(1, 7), (0, 6)], ind_per_emitter, 0.01, 5, iterations)
+        (3, 4), [(1, 4), (2, 6)], batch_size, num_emitters, iterations)
     grades = ['V4', 'V6', 'V8', 'V9']
     for grade in grades:
         me.parallel_experiment(grade, params, os.path.join('results', grade), num_reps, 6)
